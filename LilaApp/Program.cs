6db9dd5ef24403b3de70af0d43056ede2309f10d
Yaroslav Ermilov <ermilov@itglobal.ru>
2019-09-28 19:15:10 +0300

    Добавлен файл проекта

---
+++ LilaApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LilaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
