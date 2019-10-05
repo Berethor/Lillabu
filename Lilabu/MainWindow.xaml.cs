95728f7a987e42959219cd068c2b754ac9f028ff
Yaroslav Ermilov <ermilov@itglobal.ru>
2019-10-05 16:47:23 +0300

    Добавлена базовая ViewModel

--- Lilabu/MainWindow.xaml.cs
+++ Lilabu/MainWindow.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Lilabu.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lilabu
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public MainViewModel VM = new MainViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = VM;
         }
     }
 }
---
+++ Lilabu/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Lilabu.ViewModels
+{
+    public class MainViewModel: ABaseViewModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        public string Title { get => Get<string>(); set => Set(value); }
+
+        #endregion
+
+        /// <summary>
+        ///  Конструктор по-умолчанию
+        /// </summary>
+        public MainViewModel()
+        {
+            Title = "Lilabu Application";
+        }
+    }
+}
