ac6f5953dff748d0f84619871238451a30610bdd
Yaroslav Ermilov <ermilov@itglobal.ru>
2019-10-26 17:05:17 +0300

    Добавлено отображение текущей строки в редакторе входных данных

--- Lilabu/Views/FileLoader.xaml.cs
+++ Lilabu/Views/FileLoader.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Lilabu.ViewModels;
+﻿using System.Linq;
 using System.Windows.Controls;
 
 namespace Lilabu.Views
@@ -8,6 +8,18 @@ namespace Lilabu.Views
         public FileLoader()
         {
             InitializeComponent();
+
+            textBox_text.SelectionChanged += TextBox_text_SelectionChanged;
+        }
+
+        private void TextBox_text_SelectionChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if  (!(sender is TextBox textBox)) { return; }
+
+            var position = textBox.SelectionStart;
+            var lineNumber = textBox.Text.Substring(0, position).Count(c => c == '\n') + 1 ;
+
+            textBlock_lineNumber.Text = "Line: " + lineNumber;
         }
     }
 }
